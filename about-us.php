35a9b8d407687cf210d8f864cbbf20dad26afed6
MeetMakwana007 <42999594+MeetMakwana007@users.noreply.github.com>
2021-03-16 18:43:41 +0530

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,25 @@
+<?php session_start();
+$_SESSION['page']="about";
+?>
+<html>
+    <head>
+        <title>About Us</title>
+        <link rel="stylesheet" href="styleAU.css">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+    </head>
+    <body style="background-color: #2b5876">
+        <header>
+            <div class="topnav"><?php include './Header.php'; ?>
+<br>
+<div class="container">        
+        <p> <h1>🎊 <u>Mission and Vision Statement</u> 🎊</h1>
+            <h3>The sparks foundation is working to bring parity in education, making sure children have equal opportunity at success, irrespective of the financial background.
+            <br><br>    You can join The Sparks Foundation as a learner, mentor, expert, intern and make an impact on education. Learn and Share.
+           
+        <br> <br> Main motive of the foundation is to provide knowledge to all students who are really interested in learning and exploring. </h3>
+
+            </p>
+        </div>
+            </div></header>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php session_start();
+$_SESSION['page']="home";
+?>
+<html>
+    <head>
+        <title>Donate If You Can!</title>
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+    </head>
+    <body style="background-color: #2b5876">
+        <header>
+            <?php include './Header.php'; ?>
+<br>
+<div style="font-family: 'Montserrat',sans-serif;" class="container">
+                    <h1>🎈S.p.a.r.k.s. Foundation🎈</h1><br>
+                    <p><h3>We're here to help the interested people and We're having glad for this. <br>But also, We're helping to the poor people to survive their's life. <br> That's why we're accepting donations who are really eager to do.</h3></p>
+                    <br><h3 style="text-indent:15em;";> -Meet Makwana</h3>
+                </div>
+              </div>
+        </header>
+    </body>
+</html>
